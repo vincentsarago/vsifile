9d65baa95e2ed415769fbb14adc8f4d16a638d07
vincentsarago <vincent.sarago@gmail.com>
2024-07-17 00:33:06 +0200

    update tests

--- tests/test_rasterio.py
+++ tests/test_rasterio.py
@@ -5,7 +5,7 @@ import os
 import rasterio
 
 import vsifile
-from vsifile.rasterio import VSIOpener, opener
+from vsifile.rasterio import opener
 
 fixtures_dir = os.path.join(os.path.dirname(__file__), "fixtures")
 cog = os.path.join(fixtures_dir, "cog.tif")
@@ -13,13 +13,6 @@ cog = os.path.join(fixtures_dir, "cog.tif")
 
 def test_rasterio_opener():
     """Test VSIFile Local File reader."""
-    with rasterio.open(cog, opener=VSIOpener) as src:
-        profile = src.profile
-        assert profile["driver"] == "GTiff"
-        assert profile["count"] == 1
-        arr = src.read()
-        assert arr.shape == (1, 2667, 2658)
-
     with rasterio.open(cog, opener=opener) as src:
         profile = src.profile
         assert profile["driver"] == "GTiff"
@@ -27,6 +20,7 @@ def test_rasterio_opener():
         arr = src.read()
         assert arr.shape == (1, 2667, 2658)
 
+    # This won't use the read-multi-range optimization
     vsi = vsifile.filesystem("file")
     with rasterio.open(cog, opener=vsi) as src:
         profile = src.profile

3fb3f25c155b98ef6bbc69f79ab475270df1799d
github-action-benchmark <github@users.noreply.github.com>
2024-09-27 13:17:20 +0000

    add vsifile Benchmarks (pytest) benchmark result for 732a2b7d7a2a2f32b464f24fc5d8a1dc5b700c5f

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727442738290,
+  "lastUpdate": 1727443040012,
   "repoUrl": "https://github.com/vincentsarago/vsifile",
   "entries": {
     "vsifile Benchmarks": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0068610304655547915",
             "extra": "mean: 61.66106155999785 msec\nrounds: 50"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "distinct": true,
+          "id": "732a2b7d7a2a2f32b464f24fc5d8a1dc5b700c5f",
+          "message": "add benchmark in docs",
+          "timestamp": "2024-09-27T15:15:37+02:00",
+          "tree_id": "0bb83474f33b85edb5f1065d04db781c5c83f306",
+          "url": "https://github.com/vincentsarago/vsifile/commit/732a2b7d7a2a2f32b464f24fc5d8a1dc5b700c5f"
+        },
+        "date": 1727443039716,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks.py::test_preview[None]",
+            "value": 104.5597811650908,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0034476008188711895",
+            "extra": "mean: 9.563906779998774 msec\nrounds: 50"
+          },
+          {
+            "name": "tests/benchmarks.py::test_preview[op1]",
+            "value": 18.093606828375982,
+            "unit": "iter/sec",
+            "range": "stddev: 0.009766131886912754",
+            "extra": "mean: 55.268140260001246 msec\nrounds: 50"
           }
         ]
       }
